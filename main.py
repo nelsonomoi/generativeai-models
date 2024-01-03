e956af360a8a7093acef79efd903dabee96d2722
Nelson Omoi <nelson.omoi@smart.local>
2024-01-03 21:48:29 +0300

    added env file

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+from openai import OpenAI
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+api_key = os.environ.get('OPENAI_API_KEY',"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")
 
+print(api_key)
+
+client = OpenAI(
+    api_key=api_key
+)
+
+stream = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": "Say this is a test"}],
+    stream=True,
+)
+
+for chunk in stream:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end="")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(api_key)
+    print("Hello world!")
